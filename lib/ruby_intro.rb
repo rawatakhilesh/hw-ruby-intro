61005effe003a5ed0301032122d595e8903da7ff
EC2 Default User <ec2-user@ip-172-31-44-238.us-east-2.compute.internal>
2018-09-14 05:06:09 +0000

    add HW1 and log files

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,112 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  len = arr.length
+  index = 0
+  total = 0
+  while index < len do
+    total += arr[index]
+    index += 1
+  end
+  return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  len = arr.length
+  if len == 1
+    return arr[0]
+  end
+  if !arr.any?
+    return 0
+  end
+  s_arr = arr.sort
+  first = s_arr[-1]
+  second = s_arr[-2]
+  return first+second
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  len = arr.length
+  for iter in 0..(len-1) do
+    for item in (iter+1)..(len-1) do
+      # iterating each element and checking the sum against n
+      if arr[iter] + arr[item] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = "Hello, "
+  return str + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  # match vowels and non letters
+  if /^[^aeiou\d\W]/i.match(s)
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == "0"
+    return true
+  # checking for valid binary
+  elsif /[01]/.match(s)
+    # A binary multiple of 4 is left shifted by two bits i.e last two digits are 0
+    if /^[10]*00$/.match(s)
+      return true
+    else
+      return false
+    end
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  # contructor
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    if isbn.empty?
+      raise ArgumentError
+    end
+    if price <= 0
+      raise ArgumentError
+    end
+  end
+
+  # setter methods
+  def isbn=(value)
+    @isbn = value
+    if isbn.empty?
+      raise ArgumentError
+    end
+  end
+
+  def price=(value)
+    @price = value
+    if price <= 0
+      raise ArgumentError
+    end
+  end
+
+  # getter methods
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
+  def price_as_string
+    return "$#{'%.2f' % @price}"
+  end
 end
